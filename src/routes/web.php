a9ea70c554cc55fcaa743461076502f6c41bc739
unknown <asalagithub@gmail.com>
2020-11-24 16:30:10 +0200

    do

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,7 +11,7 @@ Route::group(['namespace'=>'Akrad\Bridage\Http\Controllers'],function(){
 
     Route::get('/addProject','BridageController@addProject');
 
-    Route::get('/users', 'userController@getAllUsers');
+    //Route::get('/users', 'userController@getAllUsers');
 
     Route::get('/models', 'modelController@getAllModels')->name('models');
 
